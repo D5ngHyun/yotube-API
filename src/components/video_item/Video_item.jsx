818133de4fb3be8aaba73d285006fe6ea18ab746
DongHyun Lee <dmadkr1541@naver.com>
2022-03-30 11:23:49 +0900

    youtube video list

--- src/components/video_item/Video_item.jsx
+++ src/components/video_item/Video_item.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import React from 'react';
+import styles from './Video_item.module.css';
 
-function Video_item(props) {
+function Video_item({ video }) {
+  console.log(video);
+  const { snippet } = video;
   return (
-    <div>{props.video.snippet.title}</div>
-  )
+    <li className={styles.container}>
+      <div className={styles.video}>
+        <img
+          className={styles.thumbnail}
+          src={snippet.thumbnails.medium.url}
+          alt="video thumbnail"
+        />
+        <div className={styles.metadata}>
+          <p className={styles.title}>{snippet.title}</p>
+          <p className={styles.channel}>{snippet.channelTitle}</p>
+        </div>
+      </div>
+    </li>
+  );
 }
 
-export default Video_item
+export default Video_item;
--- src/components/video_list/Video_list.jsx
+++ src/components/video_list/Video_list.jsx
@@ -1,14 +1,16 @@
-import React from 'react'
-import Video_item from '../video_item/Video_item'
+import React from 'react';
+import Video_item from '../video_item/Video_item';
+import styles from './Video_list.module.css';
 
 function Video_list(props) {
   return (
-    <ul>
-        {
-        props.videos.map(video => <Video_item video={video} key={video.id} />)
-        }
+    <ul className={styles.videos}>
+      {props.videos.map((video) => (
+        // eslint-disable-next-line react/jsx-pascal-case
+        <Video_item video={video} key={video.id} />
+      ))}
     </ul>
-  )
+  );
 }
 
-export default Video_list
+export default Video_list;
